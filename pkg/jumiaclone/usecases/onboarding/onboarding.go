054afce945c2754395e79073501f52fa32f8f482
Victorine Osewe <victorine.osewe@savannahinformatics.com>
2023-02-25 16:29:21 +0300

    add-implementation to generate otp

--- pkg/jumiaclone/usecases/onboarding/onboarding.go
+++ pkg/jumiaclone/usecases/onboarding/onboarding.go
@@ -1,6 +1,12 @@
 package onboarding
 
-import "github.com/VicOsewe/Jumia-clone/pkg/jumiaclone/domain/dto"
+import (
+	"time"
+
+	"github.com/VicOsewe/Jumia-clone/pkg/jumiaclone/domain/dto"
+	"github.com/pkg/errors"
+	"github.com/pquerna/otp/totp"
+)
 
 // OnboardingUsecase is a representation of the usecase's contract
 type OnboardingUsecase interface {
@@ -8,13 +14,14 @@ type OnboardingUsecase interface {
 
 // Onboarding sets up the onboarding's usecase layer with all the necessary dependencies
 type Onboarding struct {
+	totpOpts totp.GenerateOpts
 }
 
 // NewOnboarding initializes the Onboarding usecase instance that meets all the preconditions checks
 func NewOnboarding() *Onboarding {
-	o := &Onboarding{}
-	o.checkPreConditions()
-	return o
+	on := &Onboarding{}
+	on.checkPreConditions()
+	return on
 }
 
 func (o *Onboarding) checkPreConditions() {
@@ -26,4 +33,17 @@ func (o *Onboarding) CreateUser() (*dto.User, error) {
 	// create a record of the user in the db and mark it as a non-verified user
 	// generate and send a verification code to the user
 	return nil, nil
+}
+
+func (s *Onboarding) GenerateOTP() (string, error) {
+
+	key, err := totp.Generate(s.totpOpts)
+	if err != nil {
+		return "", errors.Wrap(err, "generateOTP")
+	}
+	code, err := totp.GenerateCode(key.Secret(), time.Now())
+	if err != nil {
+		return "", errors.Wrap(err, "generateOTP > GenerateCode")
+	}
+	return code, nil
 }
